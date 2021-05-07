67050965579408fc4062c654363823da3745ae20
alex gucci <alexgusain@gmail.com>
2021-05-07 14:28:14 -0400

    -

--- src/main.rs
+++ src/main.rs
@@ -58,20 +58,19 @@ async fn main() {
     let client2 = Client::try_default();
     let client3 = Client::try_default();
     
+    let (client, client2, client3) = tokio::join!(client,client2,client3);
     
-    let client = client.await.unwrap();//block_on(client).unwrap();
+    let client = client.unwrap();
+    let client2 = client2.unwrap();
+    let client3 = client3.unwrap();
+    
     let podapi: Api<Pod> = Api::namespaced(client, &namespace);
-    
-    let client2 = client2.await.unwrap();
     let serviceapi: Api<Service> = Api::namespaced(client2, &namespace);
-    
-    let client3 = client3.await.unwrap();
     let nodeapi: Api<Node> = Api::all(client3);
     
     
     
     let mutexmain = Arc::new(Mutex::new(Main::new(podapi, serviceapi, nodeapi)));
-    
     
     
     //listen for clients who want to be assigned a game on port 8000
@@ -88,31 +87,53 @@ async fn main() {
     
     
     
+    let copiedmutexmain1 = mutexmain.clone();
+    
+    //tick loop
+    tokio::spawn(async move {
+        
+        loop{
+            
+            println!("ticking");
+            
+            //every second
+            let sleeptime = time::Duration::from_millis(3000);
+            thread::sleep( sleeptime );
+            
+            //unlock the mutex main while handling this message
+            {
+                if let Ok(mut main) = copiedmutexmain1.lock(){
+                    main.tick();
+                }
+            }
+        }
+        
+    });
     
     
-    //a new thread that ticks the main every second
+    
     let copiedmutexmain = mutexmain.clone();
     
     
     
+    //panic if main struct poisoned
     loop {
-        println!("ticking");
+        
+        println!("checking if poisoned tick");
+        
         
         //every second
         let sleeptime = time::Duration::from_millis(3000);
         thread::sleep( sleeptime );
         
         //unlock the mutex main while handling this message
-        
         {
             if let Ok(mut main) = copiedmutexmain.lock(){
-                main.tick().await;
             }
             //if its poisoned, just panic so the pod is restarted
             else{
                 panic!("the main is poisoned. Restarting pod");
             }
-
         }
     };
     
@@ -224,7 +245,7 @@ async fn create_external_load_balancer(serviceapi: & kube::Api<k8s_openapi::api:
 fn get_available_games( state: State<Arc<Mutex<Main>>> ) -> String{
     
     println!("requesting to see the games available");
-
+    
     let game = state.inner();
     let mut game = game.lock().unwrap();
     
@@ -297,14 +318,14 @@ use std::error;
 //use std::any::Any;
 
 impl Main{
-
+    
     
     
     fn new(podapi: Api<Pod>, serviceapi: Api<Service>, nodeapi: Api<Node>) -> Main{        
         
         Main{
             pods: HashMap::new(),
-
+            
             podstomake: 15,
             
             nodeexternalip: None,
@@ -322,43 +343,43 @@ impl Main{
     //get the list of available games for the player to choose from
     //the id of each game, and the number of players in it
     fn get_available_games(&self) -> String{
-
+        
         println!("The games available are {:?}", self.pods);
         
         let mut toreturn: Vec<(u32, u8)> = Vec::new();
-
+        
         for (podid, (numberconnected,_,_,_)) in &self.pods{
-
+            
             if numberconnected < &2 {
-
+                
                 toreturn.push( (*podid, *numberconnected) );
             }
         }
-
+        
         serde_json::to_string(&toreturn).unwrap()
     }
     
     
-
-
+    
+    
     
     //a player who wants to connect to a game
     //join a game and return the address and port and password
     //as a JSON value
     fn connect_to_game(&mut self, gameid: u32) -> String{
-
-
+        
+        
         println!("Someones requesting to join game {:?}", gameid);
         
         //if theres a valid external node ip and a valid external nodeport for the pod
         if let Some(nodeexternalip) = self.nodeexternalip.clone(){
-
+            
             if let Some( ( connectedplayers, _, externalport, password) ) = self.pods.get(&gameid){
                 
                 if connectedplayers < &2{
-
+                    
                     let addressandport = "ws://".to_string() + &nodeexternalip + ":" + externalport;
-                
+                    
                     let connectedtogame = ConnectedToGame{
                         addressandport: addressandport,
                         gamepassword: password.clone(),
@@ -373,26 +394,26 @@ impl Main{
         
         return "no".to_string();        
     }
-
     
     
-    async fn tick(&mut self){
+    //this shouldnt be an async function
+    fn tick(&mut self){
+        
+        use futures::executor::block_on;
+        
         
         
         self.pods = HashMap::new();
         self.nodeexternalip = None;
         
-        
         let mut podtointernalip: HashMap<u32, String> = HashMap::new();
-        
-        
         
         //get the list of every pod with an ID and IP
         let lp = ListParams::default()
         .timeout(1)
         .labels( &("podtype=gameserver") );
         
-        let result = self.podapi.list(&lp).await.unwrap();
+        let result = block_on( self.podapi.list(&lp) ).unwrap();
         
         for item in result{
             
@@ -401,7 +422,7 @@ impl Main{
             let id = id.parse::<u32>().unwrap();
             
             println!("ids {:?}", id);
-
+            
             if let Some(ips) = item.status.clone(){
                 
                 if let Some(ip) = ips.pod_ip{
@@ -411,15 +432,18 @@ impl Main{
             }
         }
         
-
-
+        
+        
+        
+        
+        
         use tokio::time::{timeout, Duration};
         
-    
+        
         
         let mut podtopassword: HashMap<u32, String> = HashMap::new();
         let mut podtonumberofconnectedplayers: HashMap<u32, u8> = HashMap::new();
-
+        
         
         //for every pod with an internal ip
         for (podid, podip) in &podtointernalip{
@@ -427,26 +451,22 @@ impl Main{
             let address = "http://".to_string() + &podip.clone() + ":8000";
             
             
-            if let Ok(timeout) = timeout(Duration::from_secs(1), reqwest::get( &(address.clone() + "/get_players_in_game") )   ).await{
+            if let Ok(timeout) = block_on( timeout(Duration::from_millis(100), reqwest::get( &(address.clone() + "/get_players_in_game") ) ) ){
                 
                 if let Ok(result) = timeout{
-
-                    let body = result.text().await.unwrap();
-                
+                    
+                    let body = block_on( result.text() ).unwrap();
+                    
                     if let Ok(playernumb) = body.parse::<u8>(){
-
+                        
                         println!("number of players {:?}", playernumb);
                         
                         podtonumberofconnectedplayers.insert(*podid, playernumb);
                         
                         if playernumb < 2{
                             
-                            let password = reqwest::get( &(address + "/get_password") )
-                            .await
-                            .unwrap()
-                            .text()
-                            .await
-                            .unwrap();
+                            let password = block_on( block_on( reqwest::get( &(address + "/get_password") ) ).unwrap().text()).unwrap();
+                            
 
                             println!("got password {:?}", password);
                             
@@ -467,9 +487,15 @@ impl Main{
         
         
         
+        
+        
+        
+        
+        
+        
         let mut podtoexternalport: HashMap<u32, String> = HashMap::new();
         
-
+        
         //get every exposer
         let mut exposers: HashSet<u32> = HashSet::new();
         
@@ -481,7 +507,7 @@ impl Main{
         .timeout(2)
         .labels( "servicetype=gamepodexposer" );
         
-        let result = self.serviceapi.list(&lp).await.unwrap();
+        let result = block_on(self.serviceapi.list(&lp) ).unwrap();
         
         //the list of all exposers
         for item in &result{
@@ -508,30 +534,30 @@ impl Main{
             }
         }
         
-
-
-
+        
+        
+        
         for (id, internalip) in & podtointernalip{
-
+            
             if let Some(externalport) = podtoexternalport.get(&id){
-
+                
                 if let Some(numberofplayers) = podtonumberofconnectedplayers.get(&id){
-
+                    
                     if let Some(password) = podtopassword.get(&id){
-
+                        
                         self.pods.insert(*id, (*numberofplayers, internalip.clone(), externalport.clone(), password.clone() ) );
-
+                        
                     }
                 }
             }
         };
-
-
-
-
-
-
-
+        
+        
+        
+        
+        
+        
+        
         //make the load balancers that dont exist
         for x in 0..self.podstomake{
             
@@ -539,7 +565,7 @@ impl Main{
                 continue;
             }
             else{
-                create_external_load_balancer( &self.serviceapi, x).await;
+                block_on( create_external_load_balancer( &self.serviceapi, x) );
             }
         }
         
@@ -552,14 +578,14 @@ impl Main{
                 continue;
             }
             else{
-                create_gamepod(&self.podapi, x).await;
+                block_on( create_gamepod(&self.podapi, x) );
             }            
         }
         
         
         
         
-
+        
         
         //set the external ip
         self.nodeexternalip = None;
@@ -569,7 +595,7 @@ impl Main{
         let lp = ListParams::default()
         .timeout(2);
         
-        let result = self.nodeapi.list(&lp).await.unwrap();
+        let result = block_on(self.nodeapi.list(&lp)).unwrap();
         
         //for the node
         for item in &result{
@@ -589,7 +615,7 @@ impl Main{
             }
         }
     }
-
+    
     
 }
 
